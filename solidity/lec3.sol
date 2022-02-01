bd095d2cc73f0ad68acbb38fbd132e3f457d65ff
inseo24 <84627144+inseo24@users.noreply.github.com>
2022-02-01 21:23:03 +0900

    add : solidity basic

---
+++ solidity/lec3.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract lec3{
+
+    // 1 ether = 10^9 Gwei = 10^18 wei
+    // gas : 스마트 컨트랙 사용할 때 사용되는 비용
+    // 0.01 ether = 10^16wei
+
+    uint256 public value = 1 ether;
+    uint256 public value2 = 1 wei;
+    uint256 public value3 = 1 gwei;
+
+}
